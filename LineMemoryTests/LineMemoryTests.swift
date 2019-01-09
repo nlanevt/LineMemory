03a99d123013cd4e906cb6ba774bd3b0fca5d42f
Nathan Lane <nathanlane@nathans-macbook-pro-6.local>
2019-01-08 21:14:14 -0500

    Initial Commit

---
+++ LineMemoryTests/LineMemoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  LineMemoryTests.swift
+//  LineMemoryTests
+//
+//  Created by Nathan Lane on 1/8/19.
+//  Copyright © 2019 Nathan Lane. All rights reserved.
+//
+
+import XCTest
+@testable import LineMemory
+
+class LineMemoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
